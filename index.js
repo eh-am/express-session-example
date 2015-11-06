4a8d1058ff99b94e17380f68fe0aaca0806af43e
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2015-11-06 17:00:46 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+
+app.get('/', function (req, res){
+  res.send("<h1> Hello World</h1>");
+});
+
+
+app.listen(8080);
+console.log("listening on 8080");
+
+
