550f01d6e87063eb5d1b019021be8c6682fcd8f1
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2015-11-06 18:08:51 -0500

    handles basic session

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
 app.use(bodyParser());
+app.use(session({secret: '123'}));
+
 app.get('/', function (req, res){
-  res.send("<h1> Hello World</h1>");
+  res.send("<h1> Hello World " + session.userName + "</h1>" +
+           "<form action='/' method='POST'>" +
+            "<input type='text' name='userName'\>" +
+            "<button>Submit</button>" +
+           "</form>");
 });
 app.post('/', function(req, res){
   //TODO
   //test if the login is valid
-  console.log(req.body.userName);
+  
+  session.userName = req.body.userName;
+  res.redirect('/');
 });
 
 app.listen(8080);
