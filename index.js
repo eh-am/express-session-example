69aadae2286243b5c960ddeffa97fe41bf8a920f
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2015-11-06 17:55:07 -0500

    handles post request to /

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-
+app.use(bodyParser());
 app.get('/', function (req, res){
   res.send("<h1> Hello World</h1>");
 });
-
+app.post('/', function(req, res){
+  //TODO
+  //test if the login is valid
+  console.log(req.body.userName);
+});
 
 app.listen(8080);
 console.log("listening on 8080");
